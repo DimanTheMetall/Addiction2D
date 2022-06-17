cdbef029592c45856203a56e41e4ab090a9c2944
DimanTheMetall <kozlovdiman_je@yahoo.com>
2022-06-17 14:33:34 +0300

    Some changes

--- app/src/main/java/com/example/addiction2d/MainActivity.kt
+++ app/src/main/java/com/example/addiction2d/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.addiction2d
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.addiction2d.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
 }
---
+++ app/src/main/java/com/example/addiction2d/mvvm/BaseContracts.kt
@@ -0,0 +1,7 @@
+package com.example.addiction2d.mvvm
+
+interface BaseView
+
+interface BaseViewModel
+
+interface BaseModel
